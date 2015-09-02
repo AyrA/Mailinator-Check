4cbedcceead516a4b09c3547d1fd9eac359cc4a7
Kevin Gut <kevin.gut@bitm.sg>
2015-09-02 14:12:39 +0200

    This is the first release

---
+++ Mailinator-Check/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mailinator_Check
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+#if DEBUG
+            //Some test addresses. Most are mailinator
+            List<string> invalidDomains = new List<string>(new string[]
+	        {
+                "binkmail.com",
+                "bobmail.info",
+                "chammy.info",
+                "devnullmail.com",
+                "letthemeatspam.com",
+                "mailinater.com",
+                "mailinator.net",
+                "mailinator2.com",
+                "notmailinator.com",
+                "reallymymail.com",
+                "reconmail.com",
+                "safetymail.info",
+                "sendspamhere.com",
+                "sogetthis.com",
+                "spambooger.com",
+                "spamherelots.com",
+                "spamhereplease.com",
+                "spamthisplease.com",
+                "streetwisemail.com",
+                "suremail.info",
+                "thisisnotmyrealemail.com",
+                "tradermail.info",
+                "veryrealemail.com",
+                "zippymail.info"
+            });
+            foreach (string s in invalidDomains)
+            {
+                Console.WriteLine("[{0}]\t{1}", Mailinator.IsMailinator(s) ? "YES" : "NO", s);
+            }
+            Console.WriteLine("#END");
+            Console.ReadKey(true);
+            return 0;
+#else
+            int mailinatorCount = 0;
+            if (args.Length > 0)
+            {
+                foreach (string s in args)
+                {
+                    if (Mailinator.IsMailinator(s))
+                    {
+                        ++mailinatorCount;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(@"Mailinator-Check <domain> [domain [...]]
+
+Checks the given domains, if they are mailinator mailboxes or not.
+Returns the number of mailinator inboxes found. 0 if none.");
+            }
+            return mailinatorCount;
+#endif
+        }
+    }
+}
---
+++ Mailinator-Check/clsMailinator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using WinAPI.DNS;
+
+namespace Mailinator_Check
+{
+    /// <summary>
+    /// Provides checks against mailinator DNS names
+    /// </summary>
+    public static class Mailinator
+    {
+        /// <summary>
+        /// Name you consider the main mailinator mailbox
+        /// </summary>
+        private const string MAILINATOR = "mail.mailinator.com";
+
+        private static List<uint> Mailinator_IPs;
+
+        /// <summary>
+        /// Gets the mailinator IPs
+        /// </summary>
+        static Mailinator()
+        {
+            Mailinator_IPs = new List<uint>(GetRecords(MAILINATOR));
+        }
+
+        /// <summary>
+        /// Checks, if the given domain redirects to mailinator
+        /// </summary>
+        /// <param name="Host">host name</param>
+        /// <returns>true, if mailinator inbox</returns>
+        public static bool IsMailinator(string Host)
+        {
+            List<uint> Questionable_IPs = new List<uint>();
+            RecordCollection RC = Dns.Lookup(Host, Dns.QueryTypes.DNS_TYPE_MX);
+            if (RC == null)
+            {
+                return false;
+            }
+            for (int i = 0; i < RC.RecordTypes.Length; i++)
+            {
+                if (RC.RecordTypes[i] == Dns.QueryTypes.DNS_TYPE_MX)
+                {
+                    Questionable_IPs.AddRange(GetRecords(RecordCollection.ToString(((Dns.DNS_MX_DATA)RC.FoundRecords[i]).pNameExchange)));
+                }
+            }
+
+            foreach (uint Mailinator_IP in Mailinator_IPs)
+            {
+                if (Questionable_IPs.Contains(Mailinator_IP))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets A records from DNS
+        /// </summary>
+        /// <param name="Hostname">hostname to get A records from</param>
+        /// <returns>List of IP addresses (as 4 byte integers)</returns>
+        static uint[] GetRecords(string Hostname)
+        {
+            List<uint> L = new List<uint>();
+            RecordCollection RC = Dns.Lookup(Hostname, Dns.QueryTypes.DNS_TYPE_A);
+            for (int i = 0; i < RC.RecordTypes.Length; i++)
+            {
+                if (RC.RecordTypes[i] == Dns.QueryTypes.DNS_TYPE_A)
+                {
+                    L.Add(((Dns.DNS_A_DATA)RC.FoundRecords[i]).IpAddress);
+                }
+            }
+            return L.ToArray();
+        }
+    }
+}
